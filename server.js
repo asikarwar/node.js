63f1c192e987761680b53c212af5d51133911012
asikarwar <asikarwar007@gmail.com>
2017-09-09 23:12:34 +0530

    2nd &heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs =require('hbs');
 
+const port = process.env.PORT || 3000;
 
 var app = express();
 app.set('view engine', 'hbs');
@@ -16,6 +17,6 @@ app.get('/about',(req,res) =>{
 });
 
 
-app.listen(3000,() => {
-	console.log('no 3000');
+app.listen(port,() => {
+	console.log(`no ${port}`);
 });
